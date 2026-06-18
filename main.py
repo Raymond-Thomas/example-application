87dc42a66d56c242f0fd915d28ecb325f2a39d01
Raymond Thomas <rthomas2160@gmail.com>
2026-06-18 17:40:48 +0000

    Version 3.0 - Blue/Green test

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ version = os.getenv("APP_VERSION", "unknown")
 
 @app.route('/')
 def index():
-    return f'Hello Argo CD {version}!'
+    return f'Hello Argo v3.0 - Blue/Green Deployment!'
 
 app.run(host='0.0.0.0', port=8080)
