3841c98e6843e8bc959b39aac94de20d7ad27630
Raymond Thomas <rthomas2160@gmail.com>
2026-06-18 17:41:56 +0000

    Version 3.0 - Blue/Green test

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import os
 from flask import Flask
 
-
 app = Flask(__name__)
 version = os.getenv("APP_VERSION", "unknown")
 
 @app.route('/')
 def index():
-    return f'Hello Argo v3.0 - Blue/Green Deployment!'
+    return f'Hello Argo CD {version}!'
 
 app.run(host='0.0.0.0', port=8080)
